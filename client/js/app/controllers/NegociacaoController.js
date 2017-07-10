4b45a2bc93f0cd1fb07c75a1048d071cf9567956
Luiz Fernando Marcheti <luiz.marcheti@castmeta.local>
2017-07-10 13:54:51 -0300

    capitulo 4

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,18 +6,27 @@ class NegociacaoController {
 		this._data = $('#data');
 		this._quantidade = $('#quantidade');
 		this._valor = $('#valor');
+		this._negociacoes = new ListaNegociacoes();
 	}
 
 	adiciona(event){
 		event.preventDefault();
+		
+		this._negociacoes.adiciona(this._criaNegociacao());		
+		this._limparFormulario();
 
-		let data = new Date(
-			... this._data.value
-				.split('-')
-				.map((item, indice) => item - indice % 2)	
-		);
+		console.log(this._negociacoes);
+	}
 
-		let negociacao = new Negociacao(data, this._quantidade, this._valor);
-		console.log(negociacao);
+	_criaNegociacao(){
+		let data = DateHelper.textoParaData(this._data.value);
+		return new Negociacao(data, this._quantidade.value, this._valor.value);
+	}
+
+	_limparFormulario(){
+		this._data.value = '';
+		this._quantidade.value = 1;
+		this._valor.value = 0.0;
+		this._data.focus();
 	}
 }
